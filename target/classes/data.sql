0e581f1f1b43eec2eaf408e101af5343756dd5f0
Artur <artur.shabunov@gmail.com>
2017-12-29 12:55:32 +0200

    edit main page

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO User(firstname, lastname) VALUES ('Ivan','Ivanov');
+INSERT INTO User(firstname, lastname) VALUES ('Thomas','Ilves');
+INSERT INTO User(firstname, lastname) VALUES ('Anna','Voronina');
 INSERT INTO Course(title, user_id) VALUES('Eesti keel',1);
+INSERT INTO Course(title, user_id) VALUES('Eesti keel',2);
+INSERT INTO Course(title, user_id) VALUES('Eesti keel',3);
